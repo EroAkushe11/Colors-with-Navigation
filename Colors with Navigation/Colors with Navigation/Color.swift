732fa6663dcf50b4450351c71dbe91d5fbb88b3f
Ero Akushe <aerocortez@eros-macbook.local>
2018-03-24 01:28:11 -0500

    Implemented navigation with table view

---
+++ Colors with Navigation/Colors with Navigation/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  Colors with Navigation
+//
+//  Created by Ero Akushe on 3/23/18.
+//  Copyright © 2018 Oghenenyerhovwo Akushe. All rights reserved.
+//
+
+import UIKit
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
